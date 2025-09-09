0f7d1dcd3e0939950d6d3999cef3365f690c22e6
Alex Abreu <alexfavero4036@gmail.com>
2025-09-09 20:21:28 -0300

    Update PrimeiraController.php

--- primeiroprojeto/app/Http/Controllers/PrimeiraController.php
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -6,31 +6,35 @@ use Illuminate\Http\Request;
 
 class PrimeiraController extends Controller
 {
-    
-    public function teste(){
+
+    public function teste()
+    {
         return "Teste em Laravel";
     }
 
-    public function abrirForm(){
+    public function abrirForm()
+    {
         return view("exemplo");
     }
 
-    public function resposta(Request $request){
+    public function resposta(Request $request)
+    {
         $valor1 = $request->valor1;
         $valor2 = $request->valor2;
         $soma = $valor1 + $valor2;
-        return "Soma: ".$soma;
+        return "Soma: " . $soma;
     }
 
-    public function exercicio2(){
+    public function exercicio2()
+    {
         return view("exercicio2");
     }
 
-    public function respExercicio2(Request $request){
+    public function respExercicio2(Request $request)
+    {
         $valor1 = $request->valor1;
         $valor2 = $request->valor2;
         $sub = $valor1 - $valor2;
-        return "Subtração: ".$sub;
+        return "Subtração: " . $sub;
     }
-
 }
