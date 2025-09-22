d21a0ed13793f3c1ba644df0a83e9c458bc1fa8f
Alex Abreu <alexfavero4036@gmail.com>
2025-09-22 16:14:32 -0300

    exercicios até o 5

--- primeiroprojeto/app/Http/Controllers/PrimeiraController.php
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -25,6 +25,7 @@ class PrimeiraController extends Controller
         return "Soma: " . $soma;
     }
 
+    //ex2
     public function exercicio2()
     {
         return view("exercicio2");
@@ -36,5 +37,51 @@ class PrimeiraController extends Controller
         $valor2 = $request->valor2;
         $sub = $valor1 - $valor2;
         return "Subtração: " . $sub;
+    }
+
+    //ex 3
+    public function exercicio3()
+    {
+        return view("exercicio3");
+    }
+
+    public function respExercicio3(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $multi = $valor1 * $valor2;
+        return "Multiplicação: " . $multi;
+    }
+
+    //ex4
+    public function exercicio4()
+    {
+        return view("exercicio4");
+    }
+
+    public function respExercicio4(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        if ($valor2 != 0) {
+            $div = $valor1 / $valor2;
+            return "Divisão: " . $div;
+        }
+        return "O segundo valor deve ser diferente de 0";
+    }
+
+    //ex5
+    public function exercicio5()
+    {
+        return view("exercicio5");
+    }
+
+    public function respExercicio5(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $valor3 = $request->valor3;
+        $media = ($valor1 + $valor2 + $valor3) / 3;
+        return "Média = " . $media;
     }
 }
--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -14,19 +14,6 @@ Route::resource('clientes', ClienteController::class);
 //Salvar alterações - PUT /clientes/{id_cliente} -- método update
 //Excluir um registro - DELETE /clientes/{id_cliente} -- método destroy
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 Route::get('/teste', [PrimeiraController::class, "teste"]);
 
 Route::get('/exemplo', [PrimeiraController::class, "abrirForm"]);
@@ -35,3 +22,11 @@ Route::post('/exemplo_resposta', [PrimeiraController::class, "resposta"]);
 Route::get("/exercicio2", [PrimeiraController::class, "exercicio2"]);
 Route::post("/resposta_exercicio2", [PrimeiraController::class, "respExercicio2"]);
 
+Route::get("/exercicio3", [PrimeiraController::class, "exercicio3"]);
+Route::post("/resposta_exercicio3", [PrimeiraController::class, "respExercicio3"]);
+
+Route::get("/exercicio4", [PrimeiraController::class, "exercicio4"]);
+Route::post("/resposta_exercicio4", [PrimeiraController::class, "respExercicio4"]);
+
+Route::get("/exercicio5", [PrimeiraController::class, "exercicio5"]);
+Route::post("/resposta_exercicio5", [PrimeiraController::class, "respExercicio5"]);
